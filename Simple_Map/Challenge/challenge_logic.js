3fc152b19c55cdec523d464894364c8fcac68f7d
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-11-12 18:23:03 -0800

    updated

--- Simple_Map/Challenge/challenge_logic.js
+++ Simple_Map/Challenge/challenge_logic.js
@@ -29,12 +29,13 @@ let baseMaps = {
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
-let allEarthquakes = new L.LayerGroup();
+let allEarthquakes = new L.LayerGroup(streets,satelliteStreets);
 
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes
+  "Earthquakes": allEarthquakes,
+  "Satellite": satelliteStreets
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -141,7 +142,11 @@ legend.onAdd = function() {
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(() {
+
+ d3.json("https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
+   L.geoJson(data,{
+     style: mapStyle
+   }).addTo(map)
     
-  });
+});
 });
